3c00ef9c05a24b50076e31aca0267b0be833e198
Sebastian Fischer <sebf.fischer@gmail.com>
2022-02-18 06:39:56 +0100

    Update test_survivalmodels_surv_akritas.R

--- tests/testthat/test_survivalmodels_surv_akritas.R
+++ tests/testthat/test_survivalmodels_surv_akritas.R
@@ -5,5 +5,6 @@ test_that("autotest", {
   learner = LearnerSurvAkritas$new()
   expect_learner(learner)
   result = run_autotest(learner, check_replicable = FALSE)
+  print(result)
   expect_true(result, info = result$error)
 })
